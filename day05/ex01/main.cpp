31b6b9fc279cb46cf93ec41d2d22b723d1454a14
Ilyas Belkheiri <ebelkhei@e2r11p9.1337.ma>
2023-05-07 21:23:07 +0100

    added some tests

--- day05/ex01/main.cpp
+++ day05/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ebelkhei <ebelkhei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/14 17:07:44 by elias             #+#    #+#             */
-/*   Updated: 2023/05/07 21:22:47 by ebelkhei         ###   ########.fr       */
+/*   Updated: 2023/05/07 21:22:55 by ebelkhei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,26 @@
 
 int main()
 {
-
+	// Form form("Form1", 180, 10);
+	
+	// Bureaucrat a("Hmeda", 10);
+	// Form form("Form", 1, 10);
+	// std::cout << a;
+	// std::cout << form;
+	// form.beSigned(a);
+	// std::cout << form;
+	
+	// Bureaucrat a("Hmeda", 5);
+	// Form form("Form", 10, 10);
+	// std::cout << a;
+	// std::cout << form;
+	// form.beSigned(a);
+	// std::cout << form;
+	
+	// Bureaucrat a("Hmeda", 50);
+	// Form form("Form", 10, 10);
+	// std::cout << a;
+	// std::cout << form;
+	// a.signForm(form);
+	// std::cout << form;
 }
