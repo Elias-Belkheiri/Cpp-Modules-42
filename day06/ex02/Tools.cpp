fed5206cec745b6c6a1236b29a2f5f69b07cbdfd
Elias-Belkheiri <ilyasseyasser3@gmail.com>
2023-05-13 15:20:16 +0100

    Added objects type identifying

--- day06/ex02/Tools.cpp
+++ day06/ex02/Tools.cpp
@@ -6,7 +6,7 @@
 /*   By: ebelkhei <ebelkhei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/13 14:43:36 by ebelkhei          #+#    #+#             */
-/*   Updated: 2023/05/13 14:58:58 by ebelkhei         ###   ########.fr       */
+/*   Updated: 2023/05/13 15:16:00 by ebelkhei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,4 +22,46 @@ Base *generate(void)
         return (new B());
     else
         return (new C());
+}
+
+void identify(Base* p)
+{
+    if (dynamic_cast<A *>(p))
+        std::cout << "A Object" << std::endl;
+    else if (dynamic_cast<B *>(p))
+        std::cout << "B Object" << std::endl;
+    else if (dynamic_cast<C *>(p))
+        std::cout << "C Object" << std::endl;
+    else
+        std::cout << "Unknown Object" << std::endl;
+}
+
+void identify(Base& p)
+{
+    Base a;
+    try
+    {
+        a = dynamic_cast<A &>(p);
+        std::cout << "A Object" << std::endl;
+    }
+    catch (std::bad_cast &e)
+    {
+        try
+        {
+            a = dynamic_cast<B &>(p);
+            std::cout << "B Object" << std::endl;
+        }
+        catch (std::bad_cast &e)
+        {
+            try
+            {
+                a = dynamic_cast<C &>(p);
+                std::cout << "C Object" << std::endl;
+            }
+            catch (std::bad_cast &e)
+            {
+                std::cout << "Unknown Object" << std::endl;
+            }
+        }
+    }
 }
