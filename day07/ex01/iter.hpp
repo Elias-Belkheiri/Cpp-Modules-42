abfb6ff87fc50dd55b7662f47671e126fbe397dc
Elias-Belkheiri <ilyasseyasser3@gmail.com>
2023-05-18 12:10:12 +0100

    ex01

--- day07/ex01/iter.hpp
+++ day07/ex01/iter.hpp
@@ -6,14 +6,24 @@
 /*   By: ebelkhei <ebelkhei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/18 11:22:08 by ebelkhei          #+#    #+#             */
-/*   Updated: 2023/05/18 11:25:04 by ebelkhei         ###   ########.fr       */
+/*   Updated: 2023/05/18 11:43:26 by ebelkhei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ITER_HPP
 # define ITER_HPP
 
+#include <iostream>
+#include <string>
+
 template <typename T>
-T iter(T *arr, unsigned int len, void (*f)(T));
+void iter(T *arr, unsigned int len, void (*f)(T))
+{
+    if (!arr || !f)
+        return ;
+
+    for (unsigned int i = 0; i < len; i++)
+        f(arr[i]);
+}
 
 #endif
--- day07/ex01/main.cpp
+++ day07/ex01/main.cpp
@@ -1,22 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   iter.cpp                                           :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ebelkhei <ebelkhei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/18 11:25:14 by ebelkhei          #+#    #+#             */
-/*   Updated: 2023/05/18 11:34:14 by ebelkhei         ###   ########.fr       */
+/*   Created: 2023/05/18 11:40:01 by ebelkhei          #+#    #+#             */
+/*   Updated: 2023/05/18 12:10:04 by ebelkhei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "iter.hpp"
 
-void iter(T *arr, unsigned int len, void (*f)(T))
+void addTwo(int n)
 {
-    if (!arr || !f)
-        return ();
+    std::cout << n + 2 << std::endl;
+}
 
-    for (unsigned int i = 0; i < len; i++)
-        f(arr[i]);
+void upperPrint(char c)
+{
+    std::cout << (char)toupper(c) << std::endl;
+}
+
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5};
+    char str[] = "hello";
+
+    iter<int>(arr, 5, addTwo);
+    std::cout << std::endl;
+    iter<char>(str, 5, upperPrint);
+
+    return (0);   
 }
